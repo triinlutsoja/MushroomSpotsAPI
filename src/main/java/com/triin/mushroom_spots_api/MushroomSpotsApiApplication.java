02687836a728c4e69c75df03b6a6501ae0db9d7b
Triin Lutsoja <161822295+triinlutsoja@users.noreply.github.com>
2025-07-14 08:44:27 +0300

    Initial commit

---
+++ src/main/java/com/triin/mushroom_spots_api/MushroomSpotsApiApplication.java
@@ -0,0 +1,13 @@
+package com.triin.mushroom_spots_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomSpotsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MushroomSpotsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triin/mushroom_spots_api/MushroomSpotsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triin.mushroom_spots_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomSpotsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
